14536a30ea311bd36cd3b8cc2864845aa65c3d46
Willy Catão <50681413+wscatao@users.noreply.github.com>
2020-08-07 13:15:45 -0300

    Active Tab Refresh Fix

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
@@ -29,7 +29,13 @@ const useStyles = makeStyles((theme) => ({
     marginBottom: '3em',
   },
   logo: {
-    height: '7em',
+    height: '8em',
+  },
+  logoContainer: {
+    padding: '0',
+    "&:hover": {
+      backgroundColor: 'transparent'
+    }
   },
   tabContainer: {
     marginLeft: 'auto',
@@ -56,12 +62,30 @@ export default function Header(props) {
     setValue(value)
   };
 
+  useEffect(() => {
+    if (window.location.pathname === '/' && value !== 0) {
+      setValue(0);
+    } else if (window.location.pathname === '/services' && value !== 1) {
+      setValue(1);
+    } else if (window.location.pathname === '/revolution' && value !== 2) {
+      setValue(2);
+    } else if (window.location.pathname === '/about' && value !== 3) {
+      setValue(3);
+    } else if (window.location.pathname === '/contact' && value !== 4) {
+      setValue(4);
+    } else if (window.location.pathname === '/estimate' && value !== 5) {
+      setValue(5);
+    }
+  }, [value]);
+
   return (
     <>
       <ElevationScroll>
         <AppBar>
           <Toolbar disableGutters>
-            <img alt='company logo' src={logo} className={classes.logo} />
+            <Button component={Link} to="/" className={classes.logoContainer} onClick={() => setValue(0)} disableRipple>
+              <img alt='company logo' src={logo} className={classes.logo} />
+            </Button>
             <Tabs value={value} onChange={handleChange} className={classes.tabContainer} indicatorColor="primary">
               <Tab className={classes.tab} component={Link} to="/" label='Home' />
               <Tab className={classes.tab} component={Link} to="/services" label='Services' />
