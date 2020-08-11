97a2e03eff94d0c64337bd4169415d3058daa347
Willy Catão <50681413+wscatao@users.noreply.github.com>
2020-08-11 09:15:17 -0300

    Simple Menu

--- src/components/ui/Header.jsx
+++ src/components/ui/Header.jsx
@@ -6,7 +6,9 @@ import { makeStyles } from '@material-ui/styles';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Button from '@material-ui/core/Button';
-import { Link } from 'react-router-dom'; 
+import { Link } from 'react-router-dom';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
 import logo from '../../assets/logo.svg';
 
@@ -33,9 +35,9 @@ const useStyles = makeStyles((theme) => ({
   },
   logoContainer: {
     padding: '0',
-    "&:hover": {
-      backgroundColor: 'transparent'
-    }
+    '&:hover': {
+      backgroundColor: 'transparent',
+    },
   },
   tabContainer: {
     marginLeft: 'auto',
@@ -51,15 +53,27 @@ const useStyles = makeStyles((theme) => ({
     marginLeft: '50px',
     marginRight: '25px',
     height: '45px',
-  }
+  },
 }));
 
 export default function Header(props) {
   const classes = useStyles();
   const [value, setValue] = useState(0);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [open, setOpen] = useState(false);
 
   const handleChange = (e, value) => {
-    setValue(value)
+    setValue(value);
+  };
+
+  const handleClick = (e) => {
+    setAnchorEl(e.currentTarget);
+    setOpen(true);
+  };
+
+  const handleClose = (e) => {
+    setAnchorEl(null);
+    setOpen(false);
   };
 
   useEffect(() => {
@@ -83,15 +97,54 @@ export default function Header(props) {
       <ElevationScroll>
         <AppBar>
           <Toolbar disableGutters>
-            <Button component={Link} to="/" className={classes.logoContainer} onClick={() => setValue(0)} disableRipple>
+            <Button
+              component={Link}
+              to='/'
+              className={classes.logoContainer}
+              onClick={() => setValue(0)}
+              disableRipple
+            >
               <img alt='company logo' src={logo} className={classes.logo} />
             </Button>
-            <Tabs value={value} onChange={handleChange} className={classes.tabContainer} indicatorColor="primary">
-              <Tab className={classes.tab} component={Link} to="/" label='Home' />
-              <Tab className={classes.tab} component={Link} to="/services" label='Services' />
-              <Tab className={classes.tab} component={Link} to="/revolution" label='The Revolution' />
-              <Tab className={classes.tab} component={Link} to="/about" label='About Us' />
-              <Tab className={classes.tab} component={Link} to="/contact" label='Contact Us' />
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              className={classes.tabContainer}
+              indicatorColor='primary'
+            >
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to='/'
+                label='Home'
+              />
+              <Tab
+                aria-owns={anchorEl ? 'simple-menu' : undefined}
+                aria-haspopup={anchorEl ? true : undefined}
+                className={classes.tab}
+                component={Link}
+                onMouseOver={event => handleClick(event)}
+                to='/services'
+                label='Services'
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to='/revolution'
+                label='The Revolution'
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to='/about'
+                label='About Us'
+              />
+              <Tab
+                className={classes.tab}
+                component={Link}
+                to='/contact'
+                label='Contact Us'
+              />
             </Tabs>
             <Button
               variant='contained'
@@ -100,6 +153,11 @@ export default function Header(props) {
             >
               Free Estimate
             </Button>
+            <Menu id='simple-menu' anchorEl={anchorEl} open={open} onClose={handleClose} MenuListProps={{onMouseLeave: handleClose}}>
+              <MenuItem onClick={handleClose}>Custom Software Development</MenuItem>
+              <MenuItem onClick={handleClose}>Mobile App</MenuItem>
+              <MenuItem onClick={handleClose}>Website Development</MenuItem>
+            </Menu>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
