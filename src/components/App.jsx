0c1b5225ea89960da0860dd8c86864def7e25828
Willy Catão <50681413+wscatao@users.noreply.github.com>
2020-08-20 10:17:42 -0300

    Footer Setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from '@material-ui/styles';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Header from './ui/Header';
+import Footer from './ui/Footer';
 import theme from './ui/Theme';
 
 function App() {
@@ -12,7 +13,7 @@ function App() {
         <BrowserRouter>
           <Header />
           <Switch>
-            <Route exact path="/" component={() => <div>Home</div>} />
+            <Route exact path="/" component={() => <div style={{height: '2000px'}}>Home</div>} />
             <Route exact path="/services" component={() => <div>Services</div>} />
             <Route exact path="/customsoftware" component={() => <div>Custom Software</div>} />
             <Route exact path="/mobileapps" component={() => <div>Mobile Apps</div>} />
@@ -22,6 +23,7 @@ function App() {
             <Route exact path="/contact" component={() => <div>Contact</div>} />
             <Route exact path="/estimate" component={() => <div>Estimate</div>} />
           </Switch>
+          <Footer />
         </BrowserRouter>
       </ThemeProvider>
     </div>
