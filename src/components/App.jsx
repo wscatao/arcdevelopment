1bf3e2248b18f9a0ee47417a4816417471c102ef
Willy Catão <50681413+wscatao@users.noreply.github.com>
2020-09-29 10:36:46 -0300

    Fixed Animation Folder

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './ui/Header';
 import Footer from './ui/Footer';
 import theme from './ui/Theme';
+import LandingPage from './LandingPage';
 
 function App() {
   const [value, setValue] = useState(0);
@@ -21,11 +22,7 @@ function App() {
             setSelectedIndex={setSelectedIndex}
           />
           <Switch>
-            <Route
-              exact
-              path="/"
-              component={() => <div style={{ height: '2000px' }}>Home</div>}
-            />
+            <Route exact path="/" component={LandingPage} />
             <Route
               exact
               path="/services"
